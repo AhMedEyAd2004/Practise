cb36557c211f461efc411ff2d67b29783fa5520d
AhMedEyAd2004 <ahmedeyad2872004@gmail.com>
2024-09-06 20:15:39 +0300

    Add files via upload

---
+++ js/indexscript.js
@@ -0,0 +1,28 @@
+window.onscroll = () => {
+  let img = document.getElementById("title-nav").querySelectorAll("img");
+  let navText = document.getElementsByClassName("nav-link");
+  let navbar = document.getElementsByClassName("navbar")[0];
+
+  if(window.innerWidth>992)
+  {
+  if (scrollY >= 50) {
+    document.getElementsByClassName("sticky-scroll")[0].style.display = "block";
+    img[0].style.opacity = "1";
+    img[1].style.opacity = "0";
+    for (n of navText) 
+      n.classList.remove("text-white");
+    navbar.style.backgroundColor = "white";
+  } else {
+    document.getElementsByClassName("sticky-scroll")[0].style.display = "none";
+    img[0].style.opacity = "0";
+    img[1].style.opacity = "1";
+    for (n of navText) n.classList.add("text-white");
+    navbar.style.backgroundColor = "transparent";
+  }
+}
+};
+document
+  .getElementsByClassName("sticky-scroll")[0]
+  .addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
